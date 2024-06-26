a364fed8bf945ce68c0a2b3e0eb717ca361fb74d
emmameritt <emmameritt@gmail.com>
2024-06-25 21:52:26 -0500

    Part 1 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,35 +1,40 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-
+ 
+let candidateName ="";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride"; 
+let candidateAnswer = ""; 
+// runProgram()
 
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
-
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question('What is your name?');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question(question);
+  console.log(candidateAnswer , correctAnswer);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let result = "";
+  if (candidateAnswer === correctAnswer) {
+  result = "correct";
+
+   } else {
+    result= "incorrect";
+  }
+  console.log(result); 
+  
 
 
 
@@ -42,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+  console.log("Hello, " + candidateName + "!");;
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
