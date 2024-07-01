6b1383cc66683406e0692a3d431ea8b4889ec212
emmameritt <emmameritt@gmail.com>
2024-07-01 17:17:52 -0500

    Test 1-17 complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,14 @@ let candidateName ="";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride"; 
 let candidateAnswer = ""; 
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+"What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]; 
 let candidateAnswers = [];
-
+//runProgram()
 function askForName() {
   candidateName = input.question('What is your name?');
   }
@@ -21,19 +25,25 @@ return candidateAnswers; }
 
 
 function gradeQuiz(candidateAnswers){
-let result = `q1 ${correctAnswers[0]} ${candidateAnswers[0]} 
-  q2 ${correctAnswers[1]} ${candidateAnswers[1]} 
-  q3 ${correctAnswers[2]} ${candidateAnswers[2]} 
-  q4 ${correctAnswers[3]} ${candidateAnswers[3]} 
-  q5 ${correctAnswers[4]} ${candidateAnswers[4]} `;
-console.log(result);
+// let result = `q1 ${correctAnswers[0]} ${candidateAnswers[0]} 
+//   q2 ${correctAnswers[1]} ${candidateAnswers[1]} 
+//   q3 ${correctAnswers[2]} ${candidateAnswers[2]} 
+//   q4 ${correctAnswers[3]} ${candidateAnswers[3]} 
+//   q5 ${correctAnswers[4]} ${candidateAnswers[4]} `;
+// console.log(result);
   
   
 
+console.log(candidateAnswers)
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
+  for (let i = 0; i < candidateAnswers.length; i++) {
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+     grade = grade+20;
+  } 
+  };
+  console.log(grade); 
 
   return grade;
 }
